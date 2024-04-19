6528a3befda5cbce4586fd5845fdb56c4f35ae13
Константин Шишмарев <shishmaryovkonstantin@gmail.com>
2024-04-19 18:52:29 +0300

    questions

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,17 @@ import './_vendor';
 import vars from './_vars';
 import './_functions';
 import './_components';
+
+function changeActivityQuestion(el, button) {
+  document.querySelector(button).addEventListener('click', () => {
+    document.querySelector(el).classList.toggle('question--active');
+  });
+}
+
+changeActivityQuestion('.question--1', '.question--1 .question__header');
+changeActivityQuestion('.question--2', '.question--2 .question__header');
+changeActivityQuestion('.question--3', '.question--3 .question__header');
+changeActivityQuestion('.question--4', '.question--4 .question__header');
+changeActivityQuestion('.question--5', '.question--5 .question__header');
+
+
